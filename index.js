0d8ead68201cd40ba79ebd858196f91fc97558f0
Debojeet Mitra <debojeetmitra04@gmail.com>
2026-08-17 16:22:30 +0530

    change in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8080
 
 app.get('/', (req, res) => {
-    return res.json({ msg: 'Hello from a server' })
+    return res.json({ msg: 'Hello from a server by me' })
 })
 
 app.listen(PORT, () => {
